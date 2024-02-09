1a33f6e846aa8e68e5d83cc703c1746abcfa909a
Adnan774 <adnanpatel016@gmail.com>
2024-02-09 13:58:53 +0000

    Add UserPost component with like and reply functionality. Introduced UserPost component to display user posts including title, image, likes, and replies count. Features include a verified badge, relative timestamps, and interactive like action. Enhanced UI with avatars for user engagement and layered avatar effect for collaborative posts.

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,10 @@
 import UserHeader from "../components/UserHeader";
+import UserPost from "../components/UserPost";
 
 const UserPage = () => {
   return <>
     <UserHeader />
+    <UserPost  likes={1200} replies={481} postImg="/post1.png" postTitle="Lets talk about threads." />
   </>
 };
 
