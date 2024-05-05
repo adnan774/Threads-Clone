bf243b90b10929753d52c62e4da75570822357c8
Adnan774 <adnanpatel016@gmail.com>
2024-05-05 20:41:29 +0100

    Add user auth and profile management (signup, login, logout, follow/unfollow, view profile, update profile)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from 'cookie-parser';
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -8,5 +10,12 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true})); // To parse from data in the req.body
+app.use(cookieParser());
+
+// Routes
+app.use("/api/users",userRoutes)
 
 app.listen(5000, () => console.log(`Server started at http://localhost:${PORT}`));
